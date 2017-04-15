4f9f2d9fa230f9415135488a271acafaceb740bf
chentianyuan <651388057@qq.com>
2017-04-15 19:17:44 +0800

    new photo_UI

--- js/photo.js
+++ js/photo.js
@@ -1,6 +1,5 @@
-function updata(obj){
+function updata(self){
 	for(var i = 0 ; i < 3 ; i++){
-		console.log(obj.pic1);
 		var src = eval('(self.'+obj+'i'+')');
 		if(src != null){
 			$("#head").append("<img src = 'src'></img>");
